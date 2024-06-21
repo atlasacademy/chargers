3f0d5b55ab30ef06ceaca0e7f9a231e3fb455d2a
Boy on the Beach <39589879+boyonthebeach2k@users.noreply.github.com>
2024-06-21 22:30:57 +0530

    Remove Iori's unusable NP

--- src/getters/getChargers.tsx
+++ src/getters/getChargers.tsx
@@ -137,6 +137,12 @@ const getServants = async (region: "JP" | "CN" | "TW" | "KR" | "NA" = "JP"): Pro
 
     servants.splice(servants.indexOf(servants.find((serv) => serv.id === mash.id)!), 1, mashOG, mashOrtenaus);
 
+    const iori = { ...servants.find((serv) => serv.id === 106000)! };
+
+    iori.noblePhantasms = iori.noblePhantasms.filter((np) => np.id === 106001);
+
+    servants.splice(servants.indexOf(servants.find((serv) => serv.id === iori.id)!), 1, iori);
+
     return servants;
 };
 
