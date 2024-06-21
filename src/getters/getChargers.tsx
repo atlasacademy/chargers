c6d56b9cea5489457bf44efe0501f94fae0f54aa
Boy on the Beach <39589879+boyonthebeach2k@users.noreply.github.com>
2024-06-21 22:36:38 +0530

    Check undefined Iori

--- src/getters/getChargers.tsx
+++ src/getters/getChargers.tsx
@@ -137,11 +137,13 @@ const getServants = async (region: "JP" | "CN" | "TW" | "KR" | "NA" = "JP"): Pro
 
     servants.splice(servants.indexOf(servants.find((serv) => serv.id === mash.id)!), 1, mashOG, mashOrtenaus);
 
-    const iori = { ...servants.find((serv) => serv.id === 106000)! };
+    const iori = { ...servants.find((serv) => serv.id === 106000) };
 
-    iori.noblePhantasms = iori.noblePhantasms.filter((np) => np.id === 106001);
+    if (iori) {
+        iori.noblePhantasms = iori.noblePhantasms.filter((np) => np.id === 106001);
 
-    servants.splice(servants.indexOf(servants.find((serv) => serv.id === iori.id)!), 1, iori);
+        servants.splice(servants.indexOf(servants.find((serv) => serv.id === iori.id)!), 1, iori);
+    }
 
     return servants;
 };
