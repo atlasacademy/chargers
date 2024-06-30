a7d9aa2a6be7ff758892b77ffb1a9311d592ebac
Boy on the Beach <39589879+boyonthebeach2k@users.noreply.github.com>
2024-06-30 23:08:33 +0530

    Fix Iori NP fix

--- src/getters/getChargers.tsx
+++ src/getters/getChargers.tsx
@@ -139,7 +139,7 @@ const getServants = async (region: "JP" | "CN" | "TW" | "KR" | "NA" = "JP"): Pro
 
     const iori = { ...servants.find((serv) => serv.id === 106000)! };
 
-    if (iori !== undefined) {
+    if ("noblePhantasms" in iori) {
         iori.noblePhantasms = iori.noblePhantasms?.filter?.((np) => np.id === 106001) ?? iori.noblePhantasms;
 
         servants.splice(servants.indexOf(servants.find((serv) => serv.id === iori.id)!), 1, iori);
